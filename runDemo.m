6e19761c7ceefc8b2978e6c293b4ce5b25fc510b
dumcastro <eduardomdecastro@gmail.com>
2025-06-16 21:32:07 -0300

    First version of slit boundary inclusion. Try/catch blocks included for runFullExperiment.

--- runDemo.m
+++ runDemo.m
@@ -1,26 +1,34 @@
 %% THIS IS THE MAIN/DEMO SCRIPT
 clear all, clc, close all
 
-%% It should run a whole example: create graph, process graph data, solve wave, process wave data...
-Lx = 50;
-widths = [5, 5, 5];
-%angles = [0, pi - pi/12, pi+pi/12];
-angles = [0, pi - pi/30, pi+pi/3];
-kappa = 0.06;
+
+%% Main arguments
+widths = [5, 2.5, 2.5];
+angles = [0, pi - pi/24, pi + pi/3];
+kappa = 0.2;
+
+lambda_f = widths(1)/kappa;
+travel_distance = 3;
+Lx = lambda_f * (travel_distance + 1) / 2;
+
+%% Secondary parameters
+parameter_station % Go through preferred secondary arguments
+
+
 %% Testing FatGraph
 %fg = FatGraph();
 
 %% Testing create fat graph
-%createFatGraph(Lx, widths, angles)
+createFatGraph(Lx, widths, angles);
 
 %% Testing process Graph data
 
-processGraphData(Lx, widths, angles)
+%processGraphData(Lx, widths, angles)
 
 %% Testing evolveWave
 
-%evolveWave(kappa, widths, angles)
+evolveWave(kappa, widths, angles,wave_options)
 
 %% Testing processWave
 
-%processWaveData(kappa, widths, angles)
+processWaveData(kappa, widths, angles)
